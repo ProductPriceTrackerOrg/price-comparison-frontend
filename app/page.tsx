20f6bbf30279cb7906b6742910671449bc26e344
fernandonpa <158037288+fernandonpa@users.noreply.github.com>
2025-08-07 11:31:51 +0530

    set tailwind css for compatable version

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <h1>Home</h1>
+      {/* provide me to code check tailwind css work correctly green color */}
+      <h1 className="text-3xl font-bold underline text-center mt-10 text-green-500">
+        Tailwind CSS is working!
+      </h1>
     </div>
   );
 };
